1a26de054e21eeb15d891bb782f9cff592da4226
s11318045 <s11318045@students.duvalschools.org>
2021-02-18 12:32:26 -0500

    v0.01
    created d4 option

--- dice_roll.py
+++ dice_roll.py
@@ -1,2 +1,12 @@
-# dice roll library, nate mckenzie, feb 18, 2021, 12:22, V0.0
+# dice roll library, nate mckenzie, feb 18, 2021, 12:22, V0.01
+
+# D4sim
+def roll_d4(num_roll): # num_roll = argument
+    import random
+    
+    rolls = 0
+    while rolls <= num_roll:
+        result = random.randint(1,4)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls += 1
 
